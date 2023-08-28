af7d0179bf7ffb014e9547eea5639564c94396fa
Ammar Al Hasani <ammarsalem2011@gmail.com>
2023-08-28 13:12:34 +0400

    Initial commit

---
+++ bin/aaaa.dart
@@ -0,0 +1,11 @@
+import 'package:aaaa/aaaa.dart' as aaaa;
+
+void main() {
+  int total=91;
+  double? xadam;
+  xadam=(total-(10+10+8))/3;
+  double? xbele=xadam+10;
+  double xcarl=xbele+8;
+  
+  print('Tokens with Adam= ${xadam.toInt()}, tokens with Bella=${xbele.toInt()}, tokens with Bella=${xcarl.toInt()}');
+}
---
+++ test/aaaa_test.dart
@@ -0,0 +1,8 @@
+import 'package:aaaa/aaaa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
